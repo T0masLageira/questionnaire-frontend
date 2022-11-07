fe92f65cdd331e86ec204e465e2127e18e877282
T0masLageira <tomas.lageira@solera.com>
2022-11-07 12:50:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+      <h2 class="display-1">Questionnaire</h2>
+   
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
